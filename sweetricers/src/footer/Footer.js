8915da10d161641feb707bdb9affd2ec6d6d86bd
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2024-09-02 07:48:10 +0100

    :lipstick: footer

--- sweetricers/src/footer/Footer.js
+++ sweetricers/src/footer/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Row, Col } from "antd";
+import { TwitterOutlined } from "@ant-design/icons";
+
 
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <Row style={{ marginTop: "20px" }}>
+      <Col xs={8} sm={8} md={8} lg={8} style={{ border: "2px solid #ffff" }}>
+      <TwitterOutlined />
+      </Col>
+
+      <Col xs={8} sm={8} md={8} lg={8} style={{ border: "2px solid #ffff" }}>
+        jogadores online
+      </Col>
+
+      <Col xs={8} sm={8} md={8} lg={8} style={{ border: "2px solid #ffff" }}>
+        parterns
+      </Col>
+    </Row>
+  );
 }
 
 export default Footer;
