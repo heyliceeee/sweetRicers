54b5ec02bc05212c024c086883577111d2f0fe0f
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2024-09-04 22:28:36 +0100

    :lipstick: main page

--- sweetricers/src/pages/inicio/Inicio.js
+++ sweetricers/src/pages/inicio/Inicio.js
@@ -1,7 +1,132 @@
 import React from "react";
+import { Row, Col, Image } from "antd";
+import { LinkOutlined } from "@ant-design/icons";
+
+import "./Inicio.css";
+import { useState } from "react";
+
+const itemsCol1 = [
+  { id: 1, label: "1v1 multi-arenas", url: "", disable: false },
+  { id: 2, label: "retake #1", url: "", disable: false },
+  { id: 3, label: "retake #2", url: "", disable: false },
+  { id: 4, label: "retake #3", url: "", disable: false },
+  { id: 5, label: "death match ffa mirage", url: "", disable: false },
+  { id: 6, label: "death match ffa dust 2", url: "", disable: false },
+  { id: 7, label: "death match ffa saw rifles", url: "", disable: false },
+  { id: 8, label: "jailbreak", url: "", disable: true },
+  { id: 9, label: "casual fun rounds", url: "", disable: true },
+];
+
+const itemsCol2 = [
+  { id: 1, label: "6/12", disable: false },
+  { id: 2, label: "4/12", disable: false },
+  { id: 3, label: "2/12", disable: false },
+  { id: 4, label: "0/12", disable: false },
+  { id: 5, label: "0/20", disable: false },
+  { id: 6, label: "0/12", disable: false },
+  { id: 7, label: "0/47", disable: false },
+  { id: 8, label: "soon", disable: true },
+  { id: 9, label: "off", disable: true },
+];
 
 function Inicio() {
-  return <div>inicio</div>;
+  const [hoveredItem, setHoveredItem] = useState(null);
+  const [clickedItem, setClickedItem] = useState(null);
+
+  const handleMouseOver = (id) => {
+    setHoveredItem(id);
+  };
+
+  const handleMouseOut = () => {
+    setHoveredItem(null);
+  };
+
+  const handleClick = (id) => {
+    setClickedItem(id);
+  };
+
+  return (
+    <Row
+      style={{ marginTop: "20px", marginBottom: "20px" }}
+      justify="start"
+      align="middle"
+    >
+      <Col xs={20} sm={12} md={12} lg={10}>
+        {itemsCol1.map((item) => (
+          <div
+            key={item.id}
+            className={`custom-div ${item.disable ? "disabled" : ""} ${
+              hoveredItem === item.id ? "hovered" : ""
+            }
+              ${clickedItem === item.id ? "clicked" : ""}`}
+            style={{
+              border: "2px solid #5664ff",
+              borderRadius: "10px",
+              marginBottom: "10px",
+              marginRight: "10px",
+              fontSize: "20px",
+              textAlign: "center",
+              height: "50px",
+              alignItems: "center",
+              alignContent: "center",
+              color: item.disable ? "#5664ff" : "#fff",
+              backgroundColor: item.disable ? "transparent" : "#2f2b6a",
+              position: "relative",
+            }}
+            onMouseOver={() => handleMouseOver(item.id)}
+            onMouseOut={handleMouseOut}
+            onClick={() => handleClick(item.id)}
+          >
+            {item.label.toUpperCase()}
+            <LinkOutlined
+              className={`link-icon ${
+                hoveredItem === item.id ? "link-icon-visible" : ""
+              }`}
+            />
+          </div>
+        ))}
+      </Col>
+
+      <Col xs={4} sm={12} md={12} lg={2}>
+        {itemsCol2.map((item) => (
+          <div
+            key={item.id}
+            className={`custom-div ${item.disable ? "disabled" : ""} ${
+              hoveredItem === item.id ? "hovered" : ""
+            } ${clickedItem === item.id ? "clicked" : ""}`}
+            style={{
+              border: "2px solid #5664ff",
+              borderRadius: "10px",
+              marginBottom: "10px",
+              marginRight: "10px",
+              fontSize: "20px",
+              textAlign: "center",
+              height: "50px",
+              alignItems: "center",
+              alignContent: "center",
+              color: item.disable ? "#5664ff" : "#fff",
+              backgroundColor: item.disable ? "transparent" : "#2f2b6a",
+            }}
+            onMouseOver={() => handleMouseOver(item.id)}
+            onMouseOut={handleMouseOut}
+            onClick={() => handleClick(item.id)}
+          >
+            {item.label.toUpperCase()}
+          </div>
+        ))}
+      </Col>
+      <Col xs={24} sm={12} md={12} lg={12}>
+        <div
+          style={{
+            border: "2px solid #5664ff",
+            borderRadius: "10px",
+            height: "100px",
+          }}
+        ></div>
+        <div></div>
+      </Col>
+    </Row>
+  );
 }
 
 export default Inicio;
